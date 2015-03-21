0d8c532ae94c08e1f7adbb6d53d725ec4c60edff
Rahul Jadhav <rahooligan@gmail.com>
2015-03-21 23:27:51 +0000

    implemented basic ui

--- js/data-template.js
+++ js/data-template.js
@@ -1,10 +1,10 @@
 var data = {
     "user": "First Last",
-    "feeling": "happy | okay | notokay | unhappy",
+    "emotion": "happy | okay | notokay | unhappy",
     "intensity-happy": 3,
     "intensity-okay": 5,
     "intensity-not-okay": 5,
     "intensity-unhappy": 8,
-    "coments": "",
+    "comments": "",
     "timestamp": 0
 }
