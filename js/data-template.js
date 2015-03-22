c9236eec3fbc4d55a0447d8937dd36076bb58bef
Rahul Jadhav <rahooligan@gmail.com>
2015-03-22 11:34:20 +0000

    implemented rating

--- js/data-template.js
+++ js/data-template.js
@@ -1,9 +1,9 @@
 var data = {
     "user": "First Last",
-    "emotion": "happy | okay | notokay | unhappy",
+    "emotion": "happy | okay | sad | unhappy",
     "intensity-happy": 3,
     "intensity-okay": 5,
-    "intensity-not-okay": 5,
+    "intensity-sad": 5,
     "intensity-unhappy": 8,
     "comments": "",
     "timestamp": 0
--- js/smiley.js
+++ js/smiley.js
@@ -1,9 +1,29 @@
+$( document ).ready(function() {
+    var $star_rating = $('.star-rating .fa');
+
+    var SetRatingStar = function() {
+        return $star_rating.each(function() {
+            if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+                return $(this).removeClass('fa-square-o').addClass('fa-square');
+            } else {
+                return $(this).removeClass('fa-square').addClass('fa-square-o');
+            }
+        });
+    };
+
+    $star_rating.on('click', function() {
+        $star_rating.siblings('input.rating-value').val($(this).data('rating'));
+        return SetRatingStar();
+    });
+
+    SetRatingStar();
+});
 $( document ).click(function() {
     $( "#toggle" ).toggle( "slide" );
 });
 $("#feedback").submit(function(){
     $.ajax({
-        url: "https://api.mongolab.com/api/1/databases/smileydb/collections/feedback?apiKey=7HhN3sa1zLtTRKzYf34cicNQTfGWYEPd ",
+        url: "https://api.mongolab.com/api/1/databases/smileydb/collections/feedback?apiKey=7HhN3sa1zLtTRKzYf34cicNQTfGWYEPd",
         type: "POST",
         crossDomain: true,
         success: function(data) {
@@ -36,22 +56,27 @@ $( ".icon" ).on( "click", function(e) {
     if ( $( this ).hasClass( "green" ) ) {
         $(this).css("background-color", "lightgreen");
         $( "#emotion").val("happy");
+        $( ".happy").show();
     }
     if ( $( this ).hasClass( "yellow" ) ) {
         $(this).css("background-color", "yellow");
         $( "#emotion").val("okay");
+        $( ".okay").show();
     }
     if ( $( this ).hasClass( "orange" ) ) {
         $(this).css("background-color", "orange");
         $( "#emotion").val("not okay");
+        $( ".sad").show();
     }
     if ( $( this ).hasClass( "red" ) ) {
         $(this).css("background-color", "red");
         $( "#emotion").val("unhappy");
+        $( ".unhappy").show();
     }
     return false;
 });
 function rebindMouseOut(){
+    $( ".star-rating").hide();
     $( ".icon" ).css( "background-color", "" );
     $( ".icon" ).bind( "mouseout", function(){
         $( this ).css( "background-color", "" );
